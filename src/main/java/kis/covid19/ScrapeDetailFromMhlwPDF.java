dce6c4327995d5b3f65d5eabdf8e4174f2e8a1ac
kishidakohei <kohei99624@gmail.com>
2020-04-15 19:27:28 +0900

    4/15 data

--- src/main/java/kis/covid19/ScrapeDetailFromMhlwPDF.java
+++ src/main/java/kis/covid19/ScrapeDetailFromMhlwPDF.java
@@ -32,7 +32,8 @@ public class ScrapeDetailFromMhlwPDF {
         // var url = "https://www.mhlw.go.jp/content/10906000/000621070.pdf"; // 4/11
         // var url = "https://www.mhlw.go.jp/content/10906000/000621111.pdf"; // 4/12
         // var url = "https://www.mhlw.go.jp/content/10906000/000621407.pdf"; // 4/13
-        var url = "https://www.mhlw.go.jp/content/10906000/000621708.pdf"; // 4/14
+        // var url = "https://www.mhlw.go.jp/content/10906000/000621708.pdf"; // 4/14
+        var url = "https://www.mhlw.go.jp/content/10906000/000622034.pdf"; // 4/15
         
         var client = HttpClient.newHttpClient();
         var req = HttpRequest.newBuilder(URI.create(url))
