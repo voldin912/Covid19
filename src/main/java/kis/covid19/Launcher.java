9768e0f37dfb10491ea99756e6e298fed38d93e3
kishidakohei <kohei99624@gmail.com>
2020-04-21 20:47:47 +0900

    4/21 data add target

--- src/main/java/kis/covid19/Launcher.java
+++ src/main/java/kis/covid19/Launcher.java
@@ -9,7 +9,8 @@ import java.io.IOException;
 public class Launcher {
     public static void main(String[] args) throws IOException, InterruptedException {
         //var url = "https://www.mhlw.go.jp/content/10906000/000622869.pdf"; // 4/19
-        var url = "https://www.mhlw.go.jp/content/10906000/000623120.pdf"; // 4/20
+        // var url = "https://www.mhlw.go.jp/content/10906000/000623120.pdf"; // 4/20
+        var url = "https://www.mhlw.go.jp/content/10906000/000623669.pdf"; // 4/21
         
         // ScrapeDetailFromMhlwPDF.main(url);
         CreateData.main(args);
