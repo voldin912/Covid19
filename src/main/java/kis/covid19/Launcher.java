1a7605c6dddf463e02c06401daa825d61c2d8036
kishidakohei <kohei99624@gmail.com>
2020-04-23 19:52:09 +0900

    4/23 data

--- src/main/java/kis/covid19/Launcher.java
+++ src/main/java/kis/covid19/Launcher.java
@@ -12,15 +12,13 @@ public class Launcher {
         // var url = "https://www.mhlw.go.jp/content/10906000/000623120.pdf"; // 4/20
         // var url = "https://www.mhlw.go.jp/content/10906000/000623669.pdf"; // 4/21
         // var url = "https://www.mhlw.go.jp/content/10906000/000624002.pdf"; // 4/22 same as 4/21
-        var url = "https://www.mhlw.go.jp/stf/newpage_10989.html"; // 4/22
-        var newdata = false;
+        // var url = "https://www.mhlw.go.jp/stf/newpage_10989.html"; // 4/22
+        var url = "https://www.mhlw.go.jp/stf/newpage_11012.html"; // 4/23
         
-        if (newdata) {
-            if (url.endsWith(".pdf")) {
-                ScrapeDetailFromMhlwPDF.main(url);
-            } else {
-                ScrapeDetailFromMhlw.main(url);
-            }
+        if (url.endsWith(".pdf")) {
+            ScrapeDetailFromMhlwPDF.main(url);
+        } else {
+            ScrapeDetailFromMhlw.main(url);
         }
         CreateData.main(args);
     }
