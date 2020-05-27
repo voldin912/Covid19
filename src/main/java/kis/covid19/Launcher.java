a428f0a1edc36994e602c812746ed23c1501aeb6
kishidakohei <kohei99624@gmail.com>
2020-05-27 17:43:53 +0900

    5/27 data

--- src/main/java/kis/covid19/Launcher.java
+++ src/main/java/kis/covid19/Launcher.java
@@ -56,7 +56,8 @@ public class Launcher {
         // var url = "https://www.mhlw.go.jp/content/10906000/000633030.pdf"; // 5/23
         // var url = "https://www.mhlw.go.jp/content/10906000/000633053.pdf"; // 5/24
         // var url = "https://www.mhlw.go.jp/content/10906000/000633317.pdf"; // 5/25
-        var url = "https://www.mhlw.go.jp/content/10906000/000633681.pdf"; // 5/26
+        // var url = "https://www.mhlw.go.jp/content/10906000/000633681.pdf"; // 5/26
+        var url = "https://www.mhlw.go.jp/content/10906000/000634251.pdf"; // 5/27
 
         if (true) {
             // since 5/9
